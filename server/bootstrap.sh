be9f1fe0bb41efed405572ba2caf24ab36be938f
Macil Tech <maciltech@gmail.com>
2014-10-13 01:04:38 -0700

    Add vagrant file.
    Allows a vichan vm to be spun up with "vagrant up".

---
+++ server/bootstrap.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# Vagrant bootstrap file
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -y install nginx-extras imagemagick php5 php5-cli php5-curl php5-imagick php5-geoip php5-gd php5-fpm redis-server mariadb-server mariadb-client php5-mysql php5-redis emacs24-nox
+
+# Make sure any imported database is utf8mb4
+# http://mathiasbynens.be/notes/mysql-utf8mb4
+# Put in /etc/mysql/conf.d/local.cnf
+cat - <<EOF123 >/etc/mysql/conf.d/local.cnf
+[client]
+default-character-set = utf8mb4
+
+[mysql]
+default-character-set = utf8mb4
+
+[mysqld]
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
+default-storage-engine = innodb
+EOF123
+
+service mysql restart
+
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS vichan; GRANT USAGE ON *.* TO vichan@localhost IDENTIFIED BY 'vichanpass'; GRANT ALL PRIVILEGES ON vichan.* TO vichan@localhost; FLUSH PRIVILEGES;"
+
+sed \
+  -e 's/post_max_size = .*/post_max_size = 12M/' \
+  -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' \
+  -e 's/upload_max_filesize = .*/upload_max_filesize = 5M/' \
+  -i /etc/php5/fpm/php.ini
+
+service php5-fpm restart
+
+sudo install -m 775 -o www-data -g www-data -d /var/www
+ln -sf \
+  /vagrant/*.php \
+  /vagrant/js/ \
+  /vagrant/static/ \
+  /vagrant/stylesheets/ \
+  /vagrant/tools/ \
+  /vagrant/*.sql \
+  /vagrant/*.md \
+  /var/www/
+sudo install -m 775 -o www-data -g www-data -d /var/www/templates
+sudo install -m 775 -o www-data -g www-data -d /var/www/templates/cache
+ln -sf \
+  /vagrant/templates/* \
+  /var/www/templates/
+sudo install -m 775 -o www-data -g www-data -d /var/www/inc
+ln -sf \
+  /vagrant/inc/* \
+  /var/www/inc/
+rm -f /var/www/inc/instance-config.php
+cp /vagrant/inc/instance-config.php /var/www/inc/
+chown www-data /var/www/inc/instance-config.php
+
+rm -f /etc/nginx/sites-enabled/default
+ln -sf /vagrant/server/vichan.nginx /etc/nginx/sites-available/
+ln -sf /etc/nginx/sites-available/vichan.nginx /etc/nginx/sites-enabled/
+service nginx reload
+
+echo "Server set up, please browse to http://localhost:8080/install.php"
